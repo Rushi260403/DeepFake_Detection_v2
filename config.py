e7fcc58e96dccb8fe466296df3fb3ae1ed729452
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-02 19:14:56 +0530

    Phase 0: Project setup, config, gitignore

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+DATASET_PATH = r"D:\DeepFake_Dataset"
+
+REAL_VIDEOS = DATASET_PATH + r"\real_videos"
+FAKE_VIDEOS = DATASET_PATH + r"\fake_videos"
+
+MODEL_PATH = "model/deepfake_model.h5"
