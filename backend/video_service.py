bb57d1b7a2e78fd6d45be8312e4197cfc07ee96f
Rushikesh C Nikam <rushikeshnikam240@gmail.com>
2026-02-24 20:14:38 +0530

    Step 1: Fixed video_service output format

--- backend/video_service.py
+++ backend/video_service.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 sys.path.append(PROJECT_ROOT)
 
 from detection.video_predictor import predict_video
@@ -9,18 +10,10 @@ from detection.video_predictor import predict_video
 
 def analyze_video(video_path):
 
-    # Call predictor (returns dictionary)
     result = predict_video(video_path)
 
     prediction = result["prediction"]
-    confidence = result["confidence"]
-    frames_data = result["frames"]
+    confidence = float(result["confidence"])
+    frames = result["frames"]
 
-    frame_urls = []
-
-    # Extract only paths for frontend
-    for frame in frames_data[:10]:
-
-        frame_urls.append(frame["path"])
-
-    return prediction, confidence, frame_urls
+    return prediction, confidence, frames
